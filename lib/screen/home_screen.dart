099a930ca9495fbeb76f1ae8658800737bed389e
dana-0 <dnosh12344@gmail.com>
2024-08-08 04:32:24 +0300

    coffee-store

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,343 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  List dana = ["محاصيل", "مجموعات", "ماتشا", "أكواب"];
+
+  List dana2 = ["مجموعة زرقاء", "أكواب ورق"];
+
+  List<Color> bgColors = [
+    Color.fromARGB(95, 172, 172, 172),
+    Color.fromARGB(95, 172, 172, 172),
+    Color.fromARGB(95, 172, 172, 172),
+    Color.fromARGB(63, 169, 165, 165),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color.fromARGB(255, 228, 228, 228),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "المكان",
+                        style: TextStyle(fontSize: 18, color: Colors.black54),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Color.fromARGB(255, 152, 141, 131),
+                          ),
+                          Text(
+                            "جدة",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            Icons.arrow_drop_down,
+                            color: Color.fromARGB(255, 152, 141, 131),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        margin: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                                image: AssetImage("assets/user-.png"),
+                                fit: BoxFit.cover)),
+                      ),
+                      Positioned(
+                        left: 35,
+                        child: Container(
+                          padding: EdgeInsets.all(6),
+                          margin: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 1),
+                              color: Color.fromARGB(255, 109, 215, 139),
+                              shape: BoxShape.circle),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.4,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF3F3F3),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: TextFormField(
+                      decoration: InputDecoration(hintText: "ابحث هنا"),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 152, 141, 131),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Icon(
+                      Icons.filter_list,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset("assets/banner1.png"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "الأصناف",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "إظهار الكل",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 152, 141, 131),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: dana.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100,
+                    margin: EdgeInsets.only(left: 15),
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    decoration: BoxDecoration(
+                      color: bgColors[index],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                          "assets/${dana[index]}.png",
+                          width: 80,
+                          height: 80,
+                        ),
+                        Text(
+                          dana[index],
+                          style: TextStyle(fontSize: 17, color: Colors.black),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "منتشر",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "إظهار الكل",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 152, 141, 131),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 220,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: dana2.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 1.4,
+                      margin: EdgeInsets.only(
+                          left: 15, top: 5, bottom: 5, right: 5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15)),
+                            child: Image.asset(
+                              "assets/${dana2[index]}.png",
+                              height: 120,
+                              width: MediaQuery.of(context).size.width / 1.4,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      dana2[index],
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 6,
+                                    ),
+                                    Text(
+                                      "تخفيضات",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          size: 20,
+                                          color:
+                                              Color.fromARGB(255, 255, 220, 43),
+                                        ),
+                                        Text(
+                                          "4.5",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          width: 6,
+                                        ),
+                                        Text(
+                                          "999 تقييم",
+                                          style:
+                                              TextStyle(color: Colors.black54),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: Row(
+                                        children: [
+                                          Icon(
+                                            Icons.location_on,
+                                            color: Color.fromARGB(
+                                                147, 152, 141, 131),
+                                          ),
+                                          SizedBox(
+                                            width: 3,
+                                          ),
+                                          Text("1.5 KM"),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          color: Color.fromARGB(
+                                              147, 152, 141, 131),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Text(
+                                        "\SAR 103",
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.white),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
